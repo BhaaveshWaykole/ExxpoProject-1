ed999b4ba514c46cbe1edcfe5d6e0e2a775a5e43
Bhaavesh Waykole <bhaaveshw@gmail.com>
2024-04-02 11:38:05 +0530

    buttons

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { createRef } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 
@@ -8,11 +8,17 @@ const Stack = createStackNavigator();
 function ScreenA (){
   <View>
     <Text style={styles.text}>ScreenA</Text>
+    <Pressable>
+      <Text>Go to B</Text>
+    </Pressable>
   </View>
 }
 function ScreenB (){
   <View>
     <Text style={styles.text}>ScreenB</Text>
+    <Pressable>
+      <Text>Go to A</Text>
+    </Pressable>
   </View>
 }
 export default function App() {
