8a5c84c239e99680f0c371f31ac21d3c193a70f8
Bhaavesh Waykole <bhaaveshw@gmail.com>
2024-04-02 11:18:23 +0530

    second

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
+import { createRef } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from 'react-navigation/native'
 
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Screen_A" component={ScreenA}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
