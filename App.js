9ae2279883e693abec6653471ff3b1b5fe62e6d5
Bhaavesh Waykole <bhaaveshw@gmail.com>
2024-04-02 11:28:37 +0530

    stack work

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { createRef } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from 'react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
 const Stack = createStackNavigator();
+function ScreenA (){
+  <View>
+    <Text style={styles.text}>ScreenA</Text>
+  </View>
+}
+function ScreenB (){
+  <View>
+    <Text style={styles.text}>ScreenB</Text>
+  </View>
+}
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Screen_A" component={ScreenA}></Stack.Screen>
+        <Stack.Screen name="Screen_B" component={ScreenB}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
